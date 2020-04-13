92eda4070eab484d12451d4ecb414d26178f9598
Stela Capsa <stela.capsa@gmail.com>
2020-04-13 15:47:16 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,54 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  # code actions here!
+ get '/' do 
+  redirect to "/recipes"
+ end 
+ 
+ get '/recipes' do 
+ @recipes = Recipe.all 
+ erb :index
+ end 
+ 
+ get '/recipes/new' do
+    erb :new
+  end
 
+  post '/recipes' do
+    Recipe.new(
+      name: params[:name],
+      ingredients: params[:ingredients],
+      cook_time: params[:cook_time]
+    ).tap do |recipe|
+      recipe.save
+      redirect to "/recipes/#{recipe.id}"
+    end
+  end
+
+  get '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :show
+  end
+
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :edit
+  end
+
+  patch '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save
+      redirect "/recipes/#{@recipe.id}"
+  end 
+  
+ 
+
+  delete '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.delete
+    redirect  to '/recipes'
+  end
 end
